2e4287c32fc36f5a39b5213ca1020190d950f51a
Drew <aamunat2@jh.edu>
2022-05-17 16:54:26 -0400

    all tests pass for characters

--- geekle/tests/routes/characters.test.js
+++ geekle/tests/routes/characters.test.js
@@ -6,96 +6,215 @@ const CharacterDao = require("../../server/data/CharacterDao");
 const characters = new CharacterDao();
 const request = supertest(app);
 
-describe("Test character accessibility endpoints", () => {
+const endpoint = "/api/characters";
+
+describe(`Test ${endpoint} endpoints`, () => {
+
   beforeAll(async () => {
     await mongoose.connect(global.__MONGO_URI__);
-    await characters.create({
-      name: "Batman",
-      gender: "male",
-    });
   });
 
-  describe("Test /characters", () => {
+  describe(`Test GET ${endpoint}`, () => {
+    const samples = [];
 
-    describe("GET /characters", () => {
-      test("Return 201 with an array of characters", async () => {
-        const response = await request.get("/characters");
-        expect(response.body.length).toBe(1);
-        expect(response.body[0]).toHaveProperty("name");
-        expect(response.body[0]).toHaveProperty("gender");
-        expect(response.statusCode).toBe(201);
-      });
-    });
-
-    describe("POST /characters", () => {
-      test("It responds with the newly created character", async () => {
-        const newCharacter = await request
-          .post("/character")
-          .send({
-            name: "Superman",
-            gender: "male"
-          });
-    
-        // make sure we add it correctly
-        expect(newCharacter.body).toHaveProperty("id");
-        expect(newCharacter.body.name).toBe("Superman");
-        expect(newCharacter.body.gender).toBe("male");
-        expect(newCharacter.statusCode).toBe(201);
-    
-        // make sure we have 3 students now
-        const response = await request(app).get("/characters");
-        expect(response.body.length).toBe(2);
-      });
-    });
-    
-    test("Return 400 when name is missing", async () => {
-      const response = await request.post("/api/characters").send({
+    beforeAll(async () => {
+      samples[0] = await characters.create({
+        name: "Batman",
         gender: "male",
       });
-      expect(response.status).toBe(400);
-    });
 
-    test("Return 400 when gender is missing", async () => {
-      const response = await request.post("/api/characters").send({
-        name: "Batman",
-      });
-      expect(response.status).toBe(400);
-    });
-
-    test("Return 404 when id is incorrect for update", async () => {
-      const response = await request.post("/api/characters").send({
-        name: "Batman",
-        gender: "female",
-      });
-      expect(response.status).toBe(404);
-    });
-
-    test("Return 404 when id is incorrect for delete", async () => {
-      const response = await request.post("/api/characters").send({
+      samples[1] = await characters.create({
         name: "Superman",
         gender: "male",
       });
-      expect(response.status).toBe(404);
+
+      samples[2] = await characters.create({
+        name: "Skeletor",
+        gender: "male",
+      });
     });
 
-    test("Return 200 when authentication is sucessfull", async () => {
-      const response = await request.post("/api/characters").send({
+    test("Return 200 and list of characters for successful request", async () => {
+      const response = await request
+        .get(endpoint)
+      expect(response.status).toBe(200);
+      expect(response.body.data.length).toBe(3);
+    });
+
+    describe(`Test GET ${endpoint} with query parameter`, () => {
+      //Batman name
+      test("Return 200 and list of characters for successful request for batman", async () => {
+        const query = samples[0].name;
+        const response = await request
+          .get(`${endpoint}?query=${query}`)
+        expect(response.status).toBe(200);
+        const expected = samples.filter((s) => s.name.includes(query)).length;
+        expect(response.body.data.length).toBe(expected);
+      });
+
+      //Superman name
+      test("Return 200 and list of characters for successful request for superman", async () => {
+        const query = samples[1].name;
+        const response = await request
+          .get(`${endpoint}?query=${query}`)
+        expect(response.status).toBe(200);
+        const expected = samples.filter((s) => s.name.includes(query)).length;
+        expect(response.body.data.length).toBe(expected);
+      });
+
+      //Skeletor name
+      test("Return 200 and list of characters for successful request for skeletor", async () => {
+        const query = samples[2].name;
+        const response = await request
+          .get(`${endpoint}?query=${query}`)
+        expect(response.status).toBe(200);
+        const expected = samples.filter((s) => s.name.includes(query)).length;
+        expect(response.body.data.length).toBe(expected);
+      });
+
+      //gender
+      test("Return 200 and list of characters for successful request for gender", async () => {
+        const query = "male";
+        const response = await request
+          .get(`${endpoint}?query=${query}`)
+        expect(response.status).toBe(200);
+        const expected = samples.filter((s) => s.gender.includes(query)).length;
+        expect(response.body.data.length).toBe(expected);
+      });
+    });
+
+    afterAll(async () => {
+      for (const sample of samples) {
+        await characters.delete(sample._id);
+      }
+    });
+  });
+
+  describe(`Test GET ${endpoint}/:id`, () => {
+    let sample;
+
+    beforeAll(async () => {
+      sample = await characters.create({
         name: "Batman",
         gender: "male",
       });
-      expect(response.status).toBe(201);
     });
 
-    test("Return a JWT when authentication is sucessfull", async () => {
-      const response = await request.post("/api/characters").send({
-        name: "testclient",
-        gender: "testclient",
-      });
-      expect(response.body.token).toBeTruthy(); // exists and non empty!
+    test("Return 200 and the user for a given id", async () => {
+      const id = sample._id;
+      const response = await request
+        .get(`${endpoint}/${id}`)
+      expect(response.status).toBe(200);
+    });
+
+    afterAll(async () => {
+      await characters.delete(sample._id);
     });
   });
-    
-    afterAll(async () => {
-      await mongoose.connection.close();
+
+  describe(`Test POST ${endpoint}`, () => {
+    let sample;
+
+    beforeAll(async () => {
+      sample = {
+        name: "Batman",
+        gender: "male",
+      };
     });
+
+    test("Return 400 for missing name", async () => {
+      const response = await request
+        .post(endpoint)
+        .send({
+          gender: "male",
+        })
+      expect(response.status).toBe(400);
+    });
+
+    test("Return 400 for missing gender", async () => {
+      const response = await request
+        .post(endpoint)
+        .send({
+          name: "Batma ",
+        })
+      expect(response.status).toBe(400);
+    });
+
+    test("Return 201 for successful request", async () => {
+      const response = await request
+        .post(endpoint)
+        .send(sample)
+      expect(response.status).toBe(201);
+      expect(response.body.data.name).toBe(sample.name);
+      expect(response.body.data.gender).toBe(sample.gender);
+      sample._id = response.body.data._id;
+    });
+
+    afterAll(async () => {
+      await characters.delete(sample._id);
+    });
+  });
+
+  describe(`Test PUT ${endpoint}/:id`, () => {
+    let sample;
+
+    beforeAll(async () => {
+      sample = await characters.create({
+        name: "Batman",
+        gender: "male",
+      });
+    });
+
+    test("Return 404 for invalid ID", async () => {
+      const id = mongoose.Types.ObjectId().toString();
+      const response = await request
+        .put(`${endpoint}/${id}`)
+        .send({
+          name: "Superman",
+        })
+      expect(response.status).toBe(404);
+    });
+
+    test("Return 200 and updated user for successful request", async () => {
+      const response = await request
+        .put(`${endpoint}/${sample._id}`)
+        .send({
+          name: "Superman",
+        })
+      expect(response.status).toBe(200);
+    });
+
+    afterAll(async () => {
+      await characters.delete(sample._id);
+    });
+  });
+
+  describe(`Test DELETE ${endpoint}/:id`, () => {
+    let sample;
+
+    beforeAll(async () => {
+      sample = await characters.create({
+        name: "Batman",
+        gender: "male",
+      });
+    });
+
+    test("Return 404 for invalid ID", async () => {
+      const id = mongoose.Types.ObjectId().toString();
+      const response = await request
+        .delete(`${endpoint}/${id}`)
+      expect(response.status).toBe(404);
+    });
+
+    test("Return 200 for successful request", async () => {
+      const response = await request
+        .delete(`${endpoint}/${sample._id}`)
+      expect(response.status).toBe(200);
+    });
+  });
+
+  afterAll(async () => {
+    await mongoose.connection.db.dropDatabase();
+    await mongoose.connection.close();
+  });
 });
