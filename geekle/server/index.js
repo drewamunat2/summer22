df95a568d155f75e6e6eff43af12451cac7c2377
Drew <aamunat2@jh.edu>
2022-05-17 13:39:03 -0400

    refactered api to a router

--- geekle/server/index.js
+++ geekle/server/index.js
@@ -1,17 +1,20 @@
+//app
 const express = require("express");
-const nunjucks = require("nunjucks");
 const app = express();
 const port = process.env.PORT || 5050;
+
+//more app
+const nunjucks = require("nunjucks");
 const debug = require("debug")("geekle");
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
 
 //data
 const db = require("../server/data/db");
-const CharacterDao = require("../server/data/CharacterDao");
-const sampleCharacters = new CharacterDao();
-db.connect();
+const characters = require("../server/routes/characters")
+db.connect(); //mongoose
 
+//dummy array
 const charactersArray = [
   "Batman",
   "Superman",
@@ -28,14 +31,17 @@ const charactersArray = [
   "Frank N. Furter"
 ];
 
+//templating
 nunjucks.configure("views", {
   autoescape: true,
   express: app,
 });
 
+//middleware
 app.use(express.static("assets"));
 app.use(express.json());
 
+//server
 app.get("/", (req, res) => {
   res.render("index.njk", null);
 });
@@ -44,48 +50,8 @@ app.get("/game", (req, res) => {
   res.render("game.njk");
 });
 
-app.get("/api/characters", async (req, res) => {
-  const { query } = req.query;
-  const data = await sampleCharacters.readAll(query);
-  res.json({ data });
-});
-
-app.get("/api/characters/:id", async (req, res) => {
-  const { id } = req.params;
-  const data = await sampleCharacters.read(id);
-  res.json({ data: data ? data : [] });
-});
-
-app.post("/api/characters", async (req, res) => {
-  try {
-    const { name, gender } = req.body;
-    const data = await sampleCharacters.create({ name, gender });
-    res.status(201).json({ data });
-  } catch (err) {
-    res.status(err.status).json({ message: err.message });
-  }
-});
-
-app.delete("/api/characters/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const data = await sampleCharacters.delete(id);
-    res.json({ data });
-  } catch (err) {
-    res.status(err.status).json({ message: err.message });
-  }
-});
-
-app.put("/api/characters/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const { name, gender } = req.body;
-    const data = await sampleCharacters.update(id, { name, gender });
-    res.json({ data });
-  } catch (err) {
-    res.status(err.status).json({ message: err.message });
-  }
-});
+//routing
+app.use(express.static("characters"));
 
 app.listen(port, () => {
   console.log(`Express app listening at port: http://localhost:${port}/`);
---
+++ geekle/server/routes/characters.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const CharacterDao = require("../data/CharacterDao");
+
+const router = express.Router();
+const sampleCharacters = new CharacterDao();
+
+router.get("/api/characters", async (req, res) => {
+    const { query } = req.query;
+    const data = await sampleCharacters.readAll(query);
+    res.json({ data });
+});
+
+router.get("/api/characters/:id", async (req, res) => {
+    const { id } = req.params;
+    const data = await sampleCharacters.read(id);
+    res.json({ data: data ? data : [] });
+});
+
+router.post("/api/characters", async (req, res) => {
+    try {
+        const { name, gender } = req.body;
+        const data = await sampleCharacters.create({ name, gender });
+        res.status(201).json({ data });
+    } catch (err) {
+        res.status(err.status).json({ message: err.message });
+    }
+});
+
+router.delete("/api/characters/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const data = await sampleCharacters.delete(id);
+        res.json({ data });
+    } catch (err) {
+        res.status(err.status).json({ message: err.message });
+    }
+});
+
+router.put("/api/characters/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { name, gender } = req.body;
+        const data = await sampleCharacters.update(id, { name, gender });
+        res.json({ data });
+    } catch (err) {
+        res.status(err.status).json({ message: err.message });
+    }
+});
+  
